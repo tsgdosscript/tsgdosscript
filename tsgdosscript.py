7e82336fae63037f0b7b85b3c062f73066a6f25e
tsgjanfeda <selo2375@gmail.com>
2017-02-04 19:50:13 +0300

    Delete tsgdosscript.py

--- tsgdosscript.py
+++
@@ -1,4 +0,0 @@
-<br />
-<b>Warning</b>:  filesize(): stat failed for tsgdosscript.py in <b>/home/u857444701/public_html/index.php</b> on line <b>5</b><br />
-<br />
-<b>Warning</b>:  readfile(tsgdosscript.py): failed to open stream: No such file or directory in <b>/home/u857444701/public_html/index.php</b> on line <b>7</b><br />
