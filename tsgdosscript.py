a30d27b43089c3241422ef7dc535b2c8d0c51496
tsgjanfeda <selo2375@gmail.com>
2017-02-04 19:44:40 +0300

    Add files via upload

---
+++ tsgdosscript.py
@@ -0,0 +1,4 @@
+<br />
+<b>Warning</b>:  filesize(): stat failed for tsgdosscript.py in <b>/home/u857444701/public_html/index.php</b> on line <b>5</b><br />
+<br />
+<b>Warning</b>:  readfile(tsgdosscript.py): failed to open stream: No such file or directory in <b>/home/u857444701/public_html/index.php</b> on line <b>7</b><br />
